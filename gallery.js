e7922151d0132d8d2fb76742647db4a4a9f797c4
nidhip1807 <nidhi1.patel@torontomu.ca>
2024-09-21 19:16:51 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,28 +1,3 @@
-/*
-*
-* A Gallery and Imageviewer library
-* Author: Marcel Bohland Copyright 2021
-* Version: 0.1.1
-* url: https://github.com/marcelbohland
-* License: Apache License 2.0:
-*
-*
-* Copyright 2021 Marcel Bohland
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
-
 //Variablen
 added = 0;
 ImageIndex = 0;
